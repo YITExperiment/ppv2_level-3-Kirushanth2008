7da51b6c6fe84d521d1901c4abfea535dc7cccda
Kirushanth2008 <135841957+Kirushanth2008@users.noreply.github.com>
2023-06-19 12:34:19 +0530

    Add files via upload

---
+++ kaleido_spiral Size only.py
@@ -0,0 +1,21 @@
+import turtle
+
+from itertools import cycle
+colors = cycle(['red', 'orange', 'blue', 'yellow', 'green', 'White' , 'purple'])
+
+
+def draw_circle(size):
+
+    turtle.pencolor(next(colors))
+
+    turtle.circle(size)
+
+    draw_circle(size+5)
+    
+turtle.bgcolor('black')
+turtle.speed('fast')
+turtle.pensize(4)
+draw_circle(30)
+
+
+
